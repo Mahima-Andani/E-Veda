aebcc845af5c07e5ce4c9f05c6387da6d83e68b5
Muskan Dubey <muskandubey28112000@gmailcom>
2022-03-17 12:05:58 +0530

    projectconnectionproblems

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// importing necessary dependancies
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const connectDB = require('./database/connection');
 
-app.get('/', (req, res) => res.send('Hello!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+const app = express();
+
+dotenv.config({ path : 'config.env'});
+
+const port = 3000;
+connectDB();
+
+app.get('/', (req, res) => res.send('Hello!'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
